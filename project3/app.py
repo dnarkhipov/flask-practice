dc834f483893aa07898936aef4c69327130d95dd
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2020-12-04 18:26:25 +0300

    Заготовка для проекта по заданиям Главы 3 (WTF)

--- project3/app.py
+++ project3/app.py
@@ -1,29 +1,31 @@
-from flask import Flask, render_template, send_from_directory
+import os
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
+from wtforms import StringField
+from flask_wtf.csrf import CSRFProtect
+
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+SECRET_KEY = os.urandom(43)     # Создаем рандомнй ключ
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 class MyForm(FlaskForm):
     name = StringField('name')
-    email = StringField('email')
-    promocode = StringField('promocode')
-    accepted = BooleanField('accepted')
-
-
-app = Flask(__name__)
-
-
-# https://flask.palletsprojects.com/en/1.1.x/patterns/favicon/
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
-def main():
-    return ''
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/f/', methods=['GET', 'POST'])
+def my_form():
+    form = MyForm()
+    return render_template('myform.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
